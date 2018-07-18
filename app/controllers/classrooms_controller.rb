27a6650927ebb54b1b85a919edafa7013aac91d2
Zac Willmington <zac.willmington@gmail.com>
2018-07-18 18:03:15 +0000

    automatically backed up by learn

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,11 +1,10 @@
 class ClassroomsController < ApplicationController
   def show
-      binding.pry
     @classroom = Classroom.find(params[:id])
     @student = @classroom.oldest_student
   end
 
-  # def index
-  #   @classrooms = Classroom.all
-  # end
+  def index
+    @classrooms = Classroom.all
+  end
 end
